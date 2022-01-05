37e64f8d2a3499134cf32484b44b64b3d6bf176e
chrisAlgos <96036905+chrisAlgos@users.noreply.github.com>
2022-01-05 18:24:01 +0900

    Update ReorderDataInLogFiles.py

--- ReorderDataInLogFiles.py
+++ ReorderDataInLogFiles.py
@@ -1,6 +1,31 @@
 '''
 Reorder Data in Log Files
 
-(MUST) 
-https://leetcode.com/problems/reorder-data-in-log-files/
+https://leetcode.com/problems/reorder-data-in-log-files
+
+letter logs: everything else are letters
+digit logs: everything else are digits
+
+return letters + digits 
+
+where digits keep relative ordering 
+where letters are sorted by identifier IFF the rest are the same
 '''
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        
+        # divide into digits and letters
+        digits, letters = [], []
+        
+        for log in logs: 
+            arr = log.split(" ")
+            if arr[1].isdigit(): 
+                digits.append(log)
+            else: # letter 
+                letters.append(log)
+        
+        letters.sort(key=lambda x: x.split(" ")[0])
+        letters.sort(key=lambda x: x.split(" ")[1:])
+        
+        return letters+digits
