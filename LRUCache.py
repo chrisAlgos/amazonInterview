a037eee39dc3d39ff3376e365243f135951f48bc
chrisAlgos <96036905+chrisAlgos@users.noreply.github.com>
2022-01-10 06:29:27 +0900

    Update LRUCache.py

--- LRUCache.py
+++ LRUCache.py
@@ -2,6 +2,23 @@
 https://leetcode.com/problems/lru-cache/
 (MUST) 
 https://www.youtube.com/watch?v=7ABFKPK2hD4
+
+What is node? Node is just an object that stores key and its associated value. It has prev, next pointers. 
+What is cache? maps key to node that contains key & value 
+What is self.left, self.right? They indicate pointers of LRU and MRU 
+What is LL? keeps relative ordering of nodes based on LRU policy. 
+
+remove(node): removes node from the LL that keeps track of relative order of (key, value) nodes based on LRU policy 
+
+insert(node): inserts node from the right in the LL so that the node is most recently used. 
+
+get(key): purpose is to return return cache[key].val if key exists in cache else return -1.
+    Make sure to remove cache[key] in LL and add to LL.
+
+put(key, value): purpose is to insert the new node with key & value in cache. 
+    First, it checks if key exists in cache. If it does, remove key to node mapping from the cache (and from LL).
+    Then, add node with key & value to the cache (and to LL). 
+    If cache overflows, remove LRU from the cache (and from LL).
 '''
 class Node: 
     def __init__(self, key, val): 
